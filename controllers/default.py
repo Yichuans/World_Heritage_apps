b4b0e11c6cbebe21f7347b40d22fbe8cabfb6058
Yichuan Shi <yichuan.shi@outlook.com>
2015-03-18 21:35:19 +0000

    backend updated using md file instead of database, which can then be used by git to manage versions

--- controllers/default.py
+++ controllers/default.py
@@ -75,71 +75,162 @@ def data():
     """
     return dict(form=crud())
 
-## @request.restful()
-## def api():
-##     response.view = 'generic.json'
-##     import gluon.contrib.markdown.markdown2 as mkd
-## 
-## ##    def GET(tablename, wdpaid):
-## ##        if not tablename == 'person': raise HTTP(400)
-## ##        return dict(mycontent = db(db.person.wdpaid==wdpaid).select(db.person.name).first())
-## 
-##     def GET(tablename, wdpaid):
-##         if not tablename == 'person': raise HTTP(400)
-##         content_mkd = db.executesql('SELECT name FROM person WHERE wdpaid =' + str(wdpaid))[0][0]
-##         mymkd = mkd.Markdown()
-##         content_html = mymkd.convert(content_mkd)
-##         return dict(mycontent = content_html)
-##     return locals()
-
-
-# main
-@request.restful()
-def wcmcdatasheet():
-    response.view = 'generic.json'
+## FUNCTIONS ========================
+def get_wh_mkd(wdpaid):
+    """get wh datasheet in mkd by id"""
     import os
+    try:
+        with open(os.path.join(request.folder, 'mydata', str(wdpaid) + '.md'), 'r') as f:
+            content = f.read()
+        return content
+    except:
+        return ''
 
-    def GET(tablename, wdpaid):
-        if not tablename == 'wcmcdatasheet': raise HTTP(400)
-        content_mkd = db.executesql('SELECT wh FROM wcmcdatasheet WHERE wdpaid =' + str(wdpaid))[0][0]
-        return dict(mycontent = content_mkd)
+def get_last_updated(wdpaid):
+    """get information about wh datasheet by id"""
+    import os, time
+    wh_path = os.path.join(request.folder, 'mydata', str(wdpaid) + '.md')
+    wh_stat = os.stat(wh_path)
+    return 'Last uploaded on: ' + time.ctime(wh_stat.st_mtime)
 
-    return locals()
+def render_wh_mkd_to_html(wh_mkd):
+    """render wh markdown file to html"""
+    import gluon.contrib.markdown.markdown2 as md
+    mymd = md.Markdown()
+    wh_html = mymd.convert(wh_mkd)
+    return wh_html
 
-@request.restful()
-def wcmcdatasheet_render():
-    response.view = 'generic.json'
+def write_wh_mkd(wdpaid, wh_mkd):
+    """write update to wh datasheet in mkd by id, to be used in combination with GIT to track changes"""
     import os
-    import gluon.contrib.markdown.markdown2 as mkd
+    try:
+        with open(os.path.join(request.folder, 'mydata', str(wdpaid) + '.md'), 'w') as f:
+            f.write(wh_mkd)
+        return True
+    except:
+        return False
 
-    def GET(tablename, wdpaid):
-        if not tablename == 'wcmcdatasheet': raise HTTP(400)
-        content_mkd = db.executesql('SELECT wh FROM wcmcdatasheet WHERE wdpaid =' + str(wdpaid))[0][0]
-        mymkd = mkd.Markdown()
-        content_html = mymkd.convert(content_mkd)
-        return dict(mycontent = content_html)
+def bs_wh_html_add_h2_id(wh_html):
+    """use BeautifulSoup to create ID for each heading using safe strings
+    return 
+    modified HTML
+    """
+    # NEED TO INSTALL BS IN THE DEPLOYMENT ENVINRONMENT
+    from bs4 import BeautifulSoup as bs
 
-    return locals()
+    # find all h2
+    soup = bs(wh_html)
+    h2_list = soup('h2')
+
+    for each_h2 in h2_list:
+        # get the valid name
+        new_string = slugify(''.join(each_h2.strings))
+        each_h2['id'] = new_string
+
+    return soup.prettify()
+
+def bs_wh_html_h2_anchor(wh_html):
+    """use BeautifulSoup to create anchor for each heading using safe strings
+    return 
+    list of anchors
+    """
+    # NEED TO INSTALL BS IN THE DEPLOYMENT ENVINRONMENT
+    from bs4 import BeautifulSoup as bs
+
+    # find all h2
+    soup = bs(wh_html)
+    h2_list = soup('h2')
+
+    # dictionary
+    anchor_tuples = list()
+
+    for each_h2 in h2_list:
+        # get the valid name
+        anchor_name = ''.join(each_h2.strings)
+        anchor = slugify(''.join(each_h2.strings))
+        anchor_tuples.append((anchor, anchor_name))
+
+    return anchor_tuples
+
+def slugify(value):
+    """
+    Django utility
+    Converts to ASCII. Converts spaces to hyphens. Removes characters that
+    aren't alphanumerics, underscores, or hyphens. Converts to lowercase.
+    Also strips leading and trailing whitespace.
+    """
+    import re, unicodedata
+    if type(value) != str:
+        value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore').decode('ascii')
+    value = re.sub('[^\w\s-]', '', value).strip().lower()
+    value = value.replace(' ', '-')
+    return value
+
+# def test():
+#     wdpaid = 191
+#     wh_html = render_wh_mkd_to_html(get_wh_mkd(wdpaid))
+#     return bs_wh_html_add_h2_id(wh_html)
+#     # return bs_wh_html(wh_html)    
 
 
-def wh_datasheet():
-    ## this function renders each site
+## CONTROLLER ====================== 
+def wh_html():
+    # show page
     wdpaid = request.args[0]
-    return dict(wdpaid = wdpaid)
 
-def wh_datasheet_edit():
-    ## this function expose editing
-    wdpaid = request.args[0]
+    # get anchor navi
+    anchor_tuples = bs_wh_html_h2_anchor(render_wh_mkd_to_html(get_wh_mkd(wdpaid)))
 
-    record = db(db.wcmcdatasheet.wdpaid==wdpaid).select()[0]
-    form = SQLFORM(db.wcmcdatasheet, record, showid = False)
-    if form.process().accepted:
-       response.flash = 'form accepted'
-    elif form.errors:
-       response.flash = 'form has errors'
+    div_navi = DIV(_id = 'doc_navi')
+    ul = UL(_class = 'nav nav-pills nav-stacked', _role="tablist")
+
+    for anchor, anchor_name in anchor_tuples:
+        # add each anchor element
+        ul.append(LI(A(anchor_name, _href = URL('wh_html', args=[wdpaid,]) + '#' + str(anchor))))
+
+    div_navi.append(ul)
+
+    # main html
+    content_html = bs_wh_html_add_h2_id(render_wh_mkd_to_html(get_wh_mkd(wdpaid)))
+
+    div_content = DIV(_id = 'doc_content')
+    div_content.append(XML(content_html))
+
+    # last update text
+    lastupdate_text = get_last_updated(wdpaid)
+
+    return dict(mycontent = div_content, mynavi = div_navi, lastupdate = lastupdate_text)
+
+def edit_wh_html():
+    # edit page
+    wdpaid = request.args[0] # test 191
+
+    # create a form to accept updates
+    form = SQLFORM.factory(
+    Field('wdpaid', 'string',),
+    Field('markdown', 'text', length=9999999999))
+
+    # prepopulate data from text file
+    form.vars.wdpaid = wdpaid
+    form.vars.markdown = get_wh_mkd(wdpaid)
+
+    # update the record
+    if form.process(keepvalues=True).accepted:
+        if write_wh_mkd(wdpaid, request.post_vars.markdown):
+            response.flash = 'form accepted and record updated'
+        else:
+            response.flash = 'Error: not updated!'
 
     return dict(form=form)
 
-
 def hello():
     return dict()
+
+def test_last_updated():
+    wdpaid = request.args[0]
+    return get_last_updated(wdpaid)
+
+def test_view():
+    wdpaid = request.args[0]
+    return bs_wh_html_add_h2_id(render_wh_mkd_to_html(get_wh_mkd(wdpaid)))
+
